172a7cc191a2290e413582b3f41e0c496b7ac178
Purity Mwaura <purity.mwaura@student.moringaschool.com>
2022-08-25 14:18:26 +0300

    done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
+
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def not_zero?
+    [a, b, c].all?(&:positive?)
+  end
+
+  def triangle_inequality?
+    a + b > c && a + c > b && b + c > a
+  end
+
+  def validate_triangle
+    raise TriangleError unless not_zero? && triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+  end
 end
